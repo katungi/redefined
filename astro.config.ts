89aac05e44ad8462989af8bb6bceb9042843cbcc
Daniel Denis <dankatdennis@gmail.com>
2025-11-02 19:17:59 +0300

    feat: configure Vercel adapter for serverless deployment
    - Added Vercel adapter to the Astro configuration for serverless deployment capabilities.
    - Enabled web analytics within the Vercel adapter for enhanced tracking.
    - Set output mode to 'server' to optimize performance for serverless environments.

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,7 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import solidJs from "@astrojs/solid-js";
+import vercel from '@astrojs/vercel/serverless';
 import { konamiEmojiBlast } from "@konami-emoji-blast/astro";
 import { defineConfig, sharpImageService } from "astro/config";
 
@@ -8,6 +9,12 @@ import { site } from "./src/constants";
 
 // https://astro.build/config
 export default defineConfig({
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		},
+	}),
 	image: {
 		service: sharpImageService(),
 	},
@@ -15,6 +22,7 @@ export default defineConfig({
 	markdown: {
 		syntaxHighlight: "prism",
 	},
+	output: 'server',
 	prefetch: true,
 	site,
 });
